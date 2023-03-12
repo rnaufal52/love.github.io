b65339d1592be4d2b0b8f32b6fb2a1e3d513a527
rnaufal52 <116421845+rnaufal52@users.noreply.github.com>
2023-03-13 00:32:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -64,15 +64,9 @@ function draw() {
     stroke((open_animation / 10) * 360, 100, -20 + r * r_D * 120); //animation color
 
     beginShape(TRIANGLE_STRIP);
-    for (let theta = 0; theta < v[r].length; theta++) {
+   for (let theta = 0; theta < v[r].length; theta++) {
       vertex(v[r][theta].x, v[r][theta].y, v[r][theta].z);
       vertex(v[r + 1][theta].x, v[r + 1][theta].y, v[r + 1][theta].z);
-      vertex(
-        v[r + 1][theta + 1].x,
-        v[r + 1][theta + 1].y,
-        v[r + 1][theta + 1].z
-      );
-      vertex(v[r][theta + 1].x, v[r][theta + 1].y, v[r][theta + 1].z);
     }
     endShape();
   }
